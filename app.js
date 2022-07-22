2a4f343f8423a75e8bbd2b02d20def3ffda663d7
leninleon3 <104805448+leninleon3@users.noreply.github.com>
2022-07-21 22:16:40 -0300

    desafio1

---
+++ app.js
@@ -0,0 +1,22 @@
+
+let nombre = prompt ("ingrese su nombre")
+
+
+for (let i = 0; i<=5; i++){
+    if ((nombre === "sofia") || (nombre === "juan") || (nombre === "lucia") || (nombre === "ana") (nombre === "lucas")) {
+        alert ("bienvenido" + nombre);
+        else if() { alert ("usuario incorrecto");} 
+        }
+
+    if ( i === 5 ) {
+        alert ("asistencia completa");
+        else ( i != 5){
+            alert ("asistencia incompleta")
+        }
+    }
+
+    console.log ()
+
+
+
+}
