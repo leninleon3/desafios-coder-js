dcc67adfe4c3293d300e397ee28d93c49a43a450
leninleon3 <104805448+leninleon3@users.noreply.github.com>
2022-07-25 18:29:12 -0300

    desafio-obligatorio1

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
+let carrito = 0;
+    const producto1 = 30;
+    const producto2 = 50;
+    const producto3 = 35;
+    const producto4 = 60;
+    const producto5 = 45;
+    
+    function sumar (producto1, producto2, producto3, producto4, producto5) {
+    
+    for (let i=1; i <=5 ; i++) {
+        let compra = prompt ("ingrese su producto");
 
-let nombre = prompt ("ingrese su nombre")
+        if (compra === producto1 || compra === producto2 || compra === producto3 || compra === producto4 || compra === producto5) {
+            console.log (compra + compra + compra + compra + compra);}
 
-
-for (let i = 0; i<=5; i++){
-    if ((nombre === "sofia") || (nombre === "juan") || (nombre === "lucia") || (nombre === "ana") (nombre === "lucas")) {
-        alert ("bienvenido" + nombre);
-        else if() { alert ("usuario incorrecto");} 
-        }
-
-    if ( i === 5 ) {
-        alert ("asistencia completa");
-        else ( i != 5){
-            alert ("asistencia incompleta")
-        }
+            else if (compra != producto1 || compra != producto2 || compra != producto3 || compra != producto4 || compra != producto5 )
+            {console.log ("producto no seleccionado")}
+        
     }
-
-    console.log ()
-
-
-
 }
+
+
+
