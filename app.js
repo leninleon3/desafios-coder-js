0808fc0642acd38eeb400be1af3efb4c3b515f91
leninleon3 <104805448+leninleon3@users.noreply.github.com>
2022-08-05 08:34:20 -0300

    proyecto-entrega-1

--- app.js
+++ app.js
@@ -1,30 +1,82 @@
 let carrito = 0;
-    const producto1 = 30;
-    const producto2 = 50;
-    const producto3 = 35;
-    const producto4 = 60;
-    const producto5 = 45;
 
-    for (let i=1; i<=5; i++){
-        alert ("producto " + i + " disponible");
+const productos = []
+
+class carteras {
+constructor (modelo, precio, stock){
+    this.modelo = modelo;
+    this.precio = precio;
+    this.stock = stock;
+    }
+}
+
+const cartera1 = new carteras ("modelo1", 40, 10);
+productos.push (cartera1);
+
+const cartera2 = new carteras ("modelo2", 60, 3);
+productos.push (cartera2);
+
+const cartera3 = new carteras ("modelo3", 70, 5);
+productos.push (cartera3);
+
+const cartera4 = new carteras ("modelo1", 40, 2);
+productos.push (cartera4);
+
+const cartera5 = new carteras ("modelo2", 60, 6);
+productos.push (cartera5);
+
+const cartera6 = new carteras ("modelo3", 70, 2);
+productos.push (cartera6);
+
+const cartera7 = new carteras ("modelo1", 40, 1);
+productos.push (cartera7);
+
+const cartera8 = new carteras ("modelo4", 80, 10);
+productos.push (cartera8);
+
+
+
+for (let i=1; i<=8; i++){
+        alert ("cartera " + i + " disponible");
     }
     
-    function sumar () {
-        let compra = Number (prompt ("ingrese su producto"))
+    function iva () {
+        let compra = prompt ("ingrese su producto")
+        let cantidad = Number (prompt ("ingrese la cantidad deseada"))
 
-        if (compra === producto1 || compra === producto2 || compra === producto3 || compra === producto4 || compra === producto5){
+        for (const carteras of productos) {
+        
+            if (cantidad > carteras.stock) {
+            alert ("la cantidad solicitada excede nuestro stock actual");
+            break
+        }
+                
+        else if (compra === carteras.modelo){
 
-            carrito = compra * (21/100) + compra;
+            carrito = cantidad * carteras.precio * 1.21;
             alert ("su carrito es un total de " + carrito);
+
+            compra.stock - 1;
+
 
             return compra;
 
-        } else if (compra != producto1 || compra != producto2 || compra != producto3 || compra != producto4 || compra != producto5 ) {
-            alert ("producto no encontrado");
+        } else if (compra != carteras.modelo) {
+            alert ("producto no encontrado")
+            break;
         }   
     }
+}
 
-    sumar ();
+    iva ();
+
+    function stockRestante () {}
+
+
+    const modelosRestantes = productos.filter ((el) => el.modelo.includes ("modelo1"))
+    console.log (modelosRestantes);
+
+    
 
 
 
